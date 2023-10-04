fa574a678b013d9c213afdb251e02dd0bb0c4b5d
SV <SV@PM19-PC09>
2023-10-04 11:53:41 +0700

    Add project files.

---
+++ 22dh114690_NguyenHongPhuc_Lab04/MyList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22dh114690_NguyenHongPhuc_Lab04
+{
+    internal class MyList
+    {
+        private IntNode first;
+        private IntNode last;
+        public IntNode First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+        public IntNode Last
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        public int Count { get; }
+        public MyList()
+        {
+            first = null;
+            last = null;
+        }
+        public bool IsEmpty()
+        {
+            return first == null ;
+            
+        }
+        public void AddFirst(IntNode newNode)
+        {
+            if (IsEmpty())
+                first = last = newNode;
+            else
+
+            {
+                newNode.Next = first;
+                first = newNode;
+            }
+        }
+        public void AddLast(IntNode newNode)
+        {
+            if (IsEmpty())
+                first = last = newNode;
+            else
+
+            {
+                last.Next=newNode;
+                last = newNode;
+            }
+        }
+       public IntNode GetMin()
+        {
+            IntNode min = first;
+            while (first.Next != null)
+            {
+                if (first.Next.Data < min.Data)
+                    min = first.Next;
+                first = first.Next;
+            }
+            return min;
+        }
+        public IntNode GetMax()
+        {
+            IntNode max = first;
+            while (first.Next != null)
+            {
+                if (first.Next.Data > max.Data)
+                    max = first.Next;
+                first = first.Next;
+            }
+            return max;
+        }
+        public IntNode SearchX(int x)
+        {
+            
+            while (first!=null)
+            {
+                if (first.Data == x)
+                {
+                    return first;
+                }
+                first = first.Next;
+            }
+            return null;
+        }
+        public MyList GetEvenList(MyList List)
+        {
+            MyList list = new MyList();
+            while(List.first!=null)
+            {
+                if (List.first.Data % 2 == 0)
+                    list.AddLast(List.first);
+                List.first = List.first.Next;
+            }
+            return list;
+        }
+        public MyList GetOddList(MyList List)
+        {
+            MyList list = new MyList();
+            while (List.first != null)
+            {
+                if (List.first.Data % 2 != 0)
+                    list.AddLast(List.first);
+                List.first = List.first.Next;
+            }
+            return list;
+        }
+        public void ShowList()
+        {
+            IntNode p = first;
+           
+            while (p != null)
+            {
+                Console.Write("{0} -> ", p.Data);
+                p = p.Next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
+
+
---
+++ 22dh114690_NguyenHongPhuc_Lab04/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22dh114690_NguyenHongPhuc_Lab04
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyList List=new MyList();
+            int choice;
+            bool input=false;
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("1.Khoi Tao Danh Sach.");
+                Console.WriteLine("2.Xuat Danh Sach.");
+                Console.WriteLine("3.Them Vao Dau.");
+                Console.WriteLine("4.Them Vao Cuoi.");
+                Console.WriteLine("5.So Phan Tu Trong Danh Sach.");
+                Console.WriteLine("6.Tim Phan Tu Trong Danh Sach.");
+                Console.WriteLine("7.Tim Phan Tu Co Gia Tri Lon Nhat.");
+                Console.WriteLine("8.Tim Phan Tu Co Gia Tri Nho Nhat.");
+                Console.WriteLine("9.Xuat Danh Sach So Chan.");
+                Console.WriteLine("10.Xuat Danh Sach So Le.");
+               
+                Console.WriteLine("\n\n0.Thoat");
+                Console.Write("Chon Chuc Nang:");choice=int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0: return;
+                    case 1:
+                        {
+                            input = true;
+                            Console.WriteLine("Danh Sach Khoi Tao Thanh Cong.");
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (input)
+                            {
+                                Console.WriteLine("Danh Sach Co Noi Dung La: ");
+                                List.ShowList();
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break ;
+                        }
+                    case 3:
+                        {
+                            if (input)
+                            {
+                                Console.Write("Nhap Gia Tri Phan Tu:");int x=int.Parse(Console.ReadLine());
+                                IntNode NewNode = new IntNode(x);
+                                List.AddFirst(NewNode);
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (input)
+                            {
+                                Console.Write("Nhap Gia Tri Phan Tu:"); int x = int.Parse(Console.ReadLine());
+                                IntNode NewNode = new IntNode(x);
+                                List.AddLast(NewNode);
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (input)
+                            {
+                                Console.Write($"Danh Sach Co {List.Count} Phan Tu.");
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 6:
+                        {
+                            if (input)
+                            {
+                                Console.Write("Nhap Gia Tri Phan Tu Can Tim:"); int x = int.Parse(Console.ReadLine());
+                                IntNode Pos = List.SearchX(x);
+                                if (Pos == null)
+                                {
+                                    Console.WriteLine("Phan Tu Khong Ton Tai.");
+                                    break;
+                                }
+                                Console.WriteLine($"Phan Tu Ton Tai");
+
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 7:
+                        {
+                            if (input)
+                            {
+                                Console.Write("Phan Tu Lon Nhat La:" + List.GetMax().Data) ;
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 8:
+                        {
+                            if (input)
+                            {
+                                Console.Write("Phan Tu Nho Nhat La:" + List.GetMin().Data);
+                                break;
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 9:
+                        {
+                            if (input)
+                            {
+                                Console.WriteLine("Danh Sach Cac So Chan:");
+                                List.GetEvenList(List).ShowList();
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                    case 10:
+                        {
+                            if (input)
+                            {
+                                Console.WriteLine("Danh Sach Cac So Le:");
+                                List.GetOddList(List).ShowList();
+                            }
+                            Console.WriteLine("Xin Hay Khoi Tao Danh Sach Truoc.");
+                            break;
+                        }
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
